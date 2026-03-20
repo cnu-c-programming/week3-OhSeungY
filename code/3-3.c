ce3de565df1f4542587f3e3d17bbb3f09c567e50
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-03-20 10:09:06 +0900

    Update 3-3.c

---
+++ code/3-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int average(int count, ...){
+    va_list ap;
+    va_start(ap, count);
+    int average = 0;
+    for(int i=0; i<count; i++){
+        average += va_arg(ap, int);
+    }
+    average /= count;
+    return average;
+}
+
+int main() {
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
+    return 0;
+}
